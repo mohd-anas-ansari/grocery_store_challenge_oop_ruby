a5eaf6932960019d002d62b257baac346448d04c
anasansari157 <anasansari157@gmail.com>
2020-08-04 05:36:11 +0530

    initialize Counter Object with number of counts default to 0

--- cash_counter.rb
+++ cash_counter.rb
@@ -1,4 +1,10 @@
 class Counter
+    attr_accessor :counts
+
+  def initialize
+    @counts = Hash.new(0)
+  end
+  
  def take_order
     puts "Please enter your order seperated by comma."
     list_array = gets.chomp.gsub(/\s+/, "").split(",")
