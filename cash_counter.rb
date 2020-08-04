7af737172f7e31b4ee4e0f69bc67bda89d06c680
anasansari157 <anasansari157@gmail.com>
2020-08-04 06:04:35 +0530

    generate Cart on with all items

--- cash_counter.rb
+++ cash_counter.rb
@@ -19,5 +19,13 @@ class Counter
     @counts = list_array.tally
   end
 
+    def generate_cart 
+    cart = Cart.new 
+    @counts.each do | item, quantity| 
+      cart.add_to_cart (Item.new(item, quantity))
+    end
+    return cart
+  end
+
 
 end
