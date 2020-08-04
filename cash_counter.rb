1ed9dde66344d65dc38a44d2368d1a8c70267cc5
anasansari157 <anasansari157@gmail.com>
2020-08-04 05:38:40 +0530

    make hash of counts

--- cash_counter.rb
+++ cash_counter.rb
@@ -4,11 +4,16 @@ class Counter
   def initialize
     @counts = Hash.new(0)
   end
-  
+
  def take_order
     puts "Please enter your order seperated by comma."
     list_array = gets.chomp.gsub(/\s+/, "").split(",")
 
+    generate_counts list_array
+  end
+
+  def generate_counts list_array
+    @counts = list_array.tally
   end
 
 
