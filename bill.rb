a735f431ac745403fd6b3f9c9624e9bbb3f65571
anasansari157 <anasansari157@gmail.com>
2020-08-04 06:13:15 +0530

    initialize bill with total and saved amount set to 0

--- bill.rb
+++ bill.rb
@@ -2,5 +2,10 @@ require_relative "price_table"
 require_relative "cart"
 
 class Bill
+  attr_reader :total_amount, :saved_amount, :table
 
+  def initialize items, counts
+    @total_amount = 0
+    @saved_amount = 0
+  end
 end
